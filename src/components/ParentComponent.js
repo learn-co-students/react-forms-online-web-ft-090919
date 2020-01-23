ac1d82363c62f31ae1cdf09ae022fc91f6cbaf89
aminamos <26092352+aminamos@users.noreply.github.com>
2020-01-23 15:13:48 -0800

    Done.

---
+++ src/components/ParentComponent.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Form from './Form'
+import DisplayData from './DisplayData'
+ 
+export default class ParentComponent extends React.Component {
+  state = {
+    firstName: "",
+    lastName: "",
+  }
+ 
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    let formData = { firstName: this.state.firstName, lastName: this.state.lastName }
+    let dataArray = this.state.submittedData.concat(formData)
+    this.setState({submittedData: dataArray})
+  }
+ 
+  listOfSubmissions = () => {
+    return this.state.submittedData.map(data => {
+      return <div><span>{data.firstName}</span> <span>{data.lastName}</span></div>
+    })
+  }
+ 
+  render() {
+    return (
+      <div>
+        <Form
+          formData={this.state}
+          handleChange={this.handleChange}
+        />
+        <DisplayData formData={this.state} />
+      </div>
+    )
+  }
+}
